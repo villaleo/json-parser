0adb53fcf5f9f909d044eb640088f7f34939bed8
Leo Villalobos <villaleobos@gmail.com>
2022-11-09 11:18:40 -0800

    Add valueMapper

--- src/main/java/tools/JsonParser.java
+++ src/main/java/tools/JsonParser.java
@@ -3,21 +3,25 @@ package tools;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class JsonParser {
     private File file;
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
     private static class Validator {
         private final boolean valid;
-        private String fault;
+        private String message;
 
-        public Validator(boolean isValid, String fault) {
+        public static final String UNKNOWN_TYPE = "Unknown";
+        public static final String BOOLEAN_TYPE = "Boolean";
+        public static final String NUMBER_TYPE = "Number";
+        public static final String STRING_TYPE = "String";
+
+        public Validator(boolean isValid, String message) {
             this.valid = isValid;
-            this.fault = fault;
+            this.message = message;
         }
 
         public Validator(boolean isValid) {
@@ -28,13 +32,13 @@ public class JsonParser {
             return valid;
         }
 
-        public String getFault() {
-            return fault;
+        public String getMessage() {
+            return message;
         }
 
         @Override
         public String toString() {
-            return "Validator {%s, %s}".formatted(valid, fault);
+            return "Validator {%s, %s}".formatted(valid, message);
         }
     }
 
@@ -53,7 +57,7 @@ public class JsonParser {
         }
 
         if (tokenIsString(token)) {
-            return new Validator(true, "string");
+            return new Validator(true, Validator.STRING_TYPE);
         }
 
         // check if token is a number
@@ -71,18 +75,46 @@ public class JsonParser {
             // Do nothing
         }
         if (isNumber) {
-            return new Validator(true, "number");
+            return new Validator(true, Validator.NUMBER_TYPE);
         }
 
         // check if token is a boolean
         if (token.equals("true") || token.equals("false")) {
-            return new Validator(true, "boolean");
+            return new Validator(true, Validator.BOOLEAN_TYPE);
         }
-        return new Validator(false, "unknown");
+        return new Validator(false, Validator.UNKNOWN_TYPE);
     }
 
     private String removeQuotes(String token) {
-        return token.substring(1, token.length() - 1);
+        if (tokenIsString(token)) {
+            return token.substring(1, token.length() - 1);
+        }
+        return token;
+    }
+
+    private Object valueMapper(String value) {
+        Validator validator = tokenIsValidType(value);
+        if (!validator.isValid()) {
+            return null;
+        }
+
+        switch (validator.getMessage()) {
+            case Validator.STRING_TYPE:
+                if (DEBUG) {
+                    System.out.printf("\t*Found string: %s\n", value);
+                }
+                return removeQuotes(value);
+            case Validator.NUMBER_TYPE:
+                try {
+                    return Integer.parseInt(value);
+                } catch (NumberFormatException err) {
+                    return Double.parseDouble(value);
+                }
+            case Validator.BOOLEAN_TYPE:
+                return Boolean.parseBoolean(value);
+            default:
+                return null;
+        }
     }
 
     @Unfinished({"Does not support nested objects nor arrays."})
@@ -148,11 +180,11 @@ public class JsonParser {
         }
     }
 
-    public <K, V> HashMap<K, V> parseJsonToMap() {
-        var output = new HashMap<K, V>();
+    public HashMap<String, Object> parseJsonToMap() {
+        var output = new HashMap<String, Object>();
 
         if (!validJson().isValid()) {
-            System.out.printf("Invalid JSON: %s\n", validJson().getFault());
+            System.out.printf("Invalid JSON: %s\n", validJson().getMessage());
             return null;
         }
 
@@ -182,17 +214,16 @@ public class JsonParser {
             if (value.contains(",")) {
                 value = value.substring(0, value.length() - 1);
             }
-            // Remove quotes from value if string
-            if (tokenIsString(value)) {
-                value = removeQuotes(value);
-            }
 
             // Add to output
+            output.put(key, valueMapper(value));
 
             if (DEBUG) {
                 System.out.printf("\t* Adding key=`%s`, value=`%s`\n", key, value);
             }
         }
         reader.close();
+
+        return output;
     }
 }
